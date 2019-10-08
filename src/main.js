e7a39db246827926223268e26d701f33f1da1390
krzeminskamaja <mayaksheminska@gmail.com>
2019-10-08 19:20:50 +0200

    Lab1 Ex3 (Lab1 Ex2 changed)

--- src/main.js
+++ src/main.js
@@ -6,5 +6,7 @@ import AppTitle from './AppTitle'
 render(<App />, document.getElementById('reactinjectedhere'))
 render(<AppTitle />, document.getElementById('lab1ex1'))
 
-const generateArray = (n) => (Array.from(Array(n).keys()))
+const generateArray = (n) => ( Array.from(Array(n+1).keys()).filter((x)=>(x>0)))
+
+const generateRandomArray = (n) => (Array.from(Array(n).forEach((x)=>(x=1+Math.random%25))))
 
