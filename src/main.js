56f9da4800c9676b07baa588a31bd08d7b00004a
krzeminskamaja <mayaksheminska@gmail.com>
2019-10-08 22:00:01 +0200

    Lab1 Ex5

--- src/main.js
+++ src/main.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './MyApp'
 import AppTitle from './AppTitle'
+import * as data from '/Users/Maya/studia/trzecirok/react/Lab1/example.txt'
 
 render(<App />, document.getElementById('reactinjectedhere'))
 render(<AppTitle />, document.getElementById('lab1ex1'))
 
+
 const generateArray = (n) => ( Array.from(Array.from(Array(n+1).keys()).filter((x)=>(x>0))))
 
 const generateRandomArray = (n) => (Array.from({length: n},()=>1+Math.floor(Math.random()*25)))
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+
+const squareArray = (array) => (array.map(x=>{return <li>{Math.pow(x,2)}</li>}))
 
 class Appp extends React.Component{
     
     render(){
-
+        const array2 = [2,5,8,10];
         const array = [2,56,23,88,17,4];
         const filtered=array.filter(x=>x>15);
         return(
@@ -39,13 +40,16 @@ class Appp extends React.Component{
                         return <li>{x}</li>;
                     })}
                 </ul>
+                <h3>Lab1 Ex5</h3>
+                <ul>
+                    {squareArray(array2)}
+                </ul>
             </div>
             
         );
     }
 }
-
-
-
-
 render(<Appp/>,document.getElementById('largerthan15'))
+
+
+
