35a90bc1d312e30beeff4a21bf138742e32f59f4
krzeminskamaja <mayaksheminska@gmail.com>
2019-10-13 11:49:51 +0200

    Lab1 Ex7

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 import { render } from 'react-dom'
 import App from './MyApp'
 import AppTitle from './AppTitle'
-import * as data from '/Users/Maya/studia/trzecirok/react/Lab1/example.txt'
+import * as data from '/Users/Maya/studia/trzecirok/react/Lab1/example.json'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 render(<App />, document.getElementById('reactinjectedhere'))
 render(<AppTitle />, document.getElementById('lab1ex1'))
@@ -13,6 +16,30 @@ const generateArray = (n) => ( Array.from(Array.from(Array(n+1).keys()).filter((
 const generateRandomArray = (n) => (Array.from({length: n},()=>1+Math.floor(Math.random()*25)))
 
 const squareArray = (array) => (array.map(x=>{return <li>{Math.pow(x,2)}</li>}))
+
+class Students extends React.Component{
+    click1(){
+        return(
+            <div>
+                {data.map((detail,index)=>{
+                    return <div>
+                        <p>{detail.teacherName}</p>
+                    </div>
+                })}
+            </div>
+        )
+    }
+    render(){
+        return(
+            <div>
+                <h3>Lab1 Ex7</h3>
+                <Button variant="success" block>All students</Button>
+                <Button variant="light" block>Sort students</Button>
+                <Button variant="dark" block>Old students</Button>
+            </div>
+        );
+    }
+}
 
 class Appp extends React.Component{
     
@@ -44,12 +71,13 @@ class Appp extends React.Component{
                 <ul>
                     {squareArray(array2)}
                 </ul>
+                
             </div>
             
         );
     }
 }
 render(<Appp/>,document.getElementById('largerthan15'))
-
+render(<Students/>,document.getElementById('students'))
 
 
